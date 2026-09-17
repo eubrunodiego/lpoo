6cc5d0a7472c6699a507daa9a17ee6367f120c79
Bruno Pereira <86216391+eubrunodiego@users.noreply.github.com>
2026-09-17 16:11:49 -0300

    Enhance HelloWorld with console output option
    Added a console output option to the HelloWorld program.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,22 @@ import javax.swing.JOptionPane;
 public class HelloWorld {
 
     public static void main(String[] args){
-
+        
+        /*
+        * 1ª Opção
+        * Utilizando interface gráfica
+        */
         JOptionPane.showMessageDialog(
             null,
             "Hello World!",
             "Minha primeira janela",
             JOptionPane.ERROR_MESSAGE);
+
+        /*
+        * 2ª Opção
+        * Usando a saída padrão (texto no terminal)
+        */
+        System.out.println("Seu 'Hello World' funcionou!");
+
     }
 }
